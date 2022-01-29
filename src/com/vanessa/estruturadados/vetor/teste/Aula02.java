941ded8b93f4c4594e0ef4e3bd66d911674d591b
VanessaYoshida <vanessayoshida92@gmail.com>
2022-01-28 21:35:49 -0300

    Arquivos iniciais da aula de estrutura de dados

---
+++ src/com/vanessa/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,14 @@
+package com.vanessa.estruturadados.vetor.teste;
+
+import com.vanessa.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+	
+	public static void main(String[] args) {
+		Vetor vetor = new Vetor(5);
+		
+		int i=0;
+		i++;
+	}
+
+}
