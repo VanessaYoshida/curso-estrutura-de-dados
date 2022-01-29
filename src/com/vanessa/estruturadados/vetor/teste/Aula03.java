1e4619dbe0fdf6721547d799ed7d4b1e7f8745d1
VanessaYoshida <vanessayoshida92@gmail.com>
2022-01-28 22:19:39 -0300

    Adicionando teste de adicionar dados no vetor

---
+++ src/com/vanessa/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,16 @@
+package com.vanessa.estruturadados.vetor.teste;
+
+import com.vanessa.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(5);
+		
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+
+	}
+
+}
