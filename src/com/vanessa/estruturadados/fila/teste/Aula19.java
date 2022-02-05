f1033b3eb65378d09e8b630a05ea4d1175e95ab4
VanessaYoshida <vanessayoshida92@gmail.com>
2022-02-04 22:06:56 -0300

    aulas sobre fila

---
+++ src/com/vanessa/estruturadados/fila/teste/Aula19.java
@@ -0,0 +1,15 @@
+package com.vanessa.estruturadados.fila.teste;
+
+import com.vanessa.estruturadados.fila.Fila;
+
+public class Aula19 {
+
+	public static void main(String[] args) {
+		
+		Fila<Integer> fila = new Fila<>();
+		
+		System.out.println(fila.estaVazia()); //true
+		System.out.println(fila.tamanho()); // 0
+	}
+
+}
---
+++ src/com/vanessa/estruturadados/fila/teste/Interface1.java
@@ -0,0 +1,8 @@
+package com.vanessa.estruturadados.fila.teste;
+
+public interface Interface1 {
+	
+	public void adiciona();
+	
+	public void remove();
+}
